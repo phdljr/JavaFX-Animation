526bf8b3f1a7ec1b6a9e81ce405e72d072e0eeb6
phdljr <phdljr@naver.com>
2022-01-15 02:25:11 +0900

    Add opacity animation.

--- src/controller/LoginLayoutController.java
+++ src/controller/LoginLayoutController.java
@@ -3,13 +3,17 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class LoginLayoutController implements Initializable{
 	@FXML
@@ -24,7 +28,19 @@ public class LoginLayoutController implements Initializable{
 	public void handleBtnMain(ActionEvent e) {
 		try {
 			StackPane root = (StackPane)btn_main.getScene().getRoot();
-			root.getChildren().remove(login);
+			
+			login.setOpacity(1);
+			
+			Timeline tl = new Timeline();
+			KeyValue kv = new KeyValue(login.opacityProperty(), 0);
+			KeyFrame kf = new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent arg0) {
+					root.getChildren().remove(login);
+				}
+			}, kv);
+			tl.getKeyFrames().add(kf);
+			tl.play();
 		}
 		catch(Exception e2) {
 			e2.printStackTrace();
--- src/controller/MainLayoutController.java
+++ src/controller/MainLayoutController.java
@@ -3,6 +3,9 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class MainLayoutController implements Initializable{
 	@FXML
@@ -25,6 +29,14 @@ public class MainLayoutController implements Initializable{
 			Parent login = FXMLLoader.load(getClass().getClassLoader().getResource("layout/LoginLayout.fxml"));
 			StackPane root = (StackPane)btn_login.getScene().getRoot();
 			root.getChildren().add(login);
+			
+			login.setTranslateX(350);
+			
+			Timeline timeline = new Timeline();
+			KeyValue keyValue = new KeyValue(login.translateXProperty(), 0);
+			KeyFrame keyFrame = new KeyFrame(Duration.millis(100), keyValue);
+			timeline.getKeyFrames().add(keyFrame);
+			timeline.play();
 		}
 		catch(Exception e2) {
 			e2.printStackTrace();
